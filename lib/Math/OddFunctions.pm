55bf9331206cefd554ddba3dc4033dd5ec5bc94b
Solomon Foster <colomon@gmail.com>
2015-02-01 22:35:47 -0500

    Add comment which may be useful.

--- lib/Math/OddFunctions.pm
+++ lib/Math/OddFunctions.pm
@@ -1,5 +1,7 @@
 use v6;
 use NativeCall;
 
+# libSystem should maybe be libm on Linux?
+
 sub erf(num) returns num is native('libSystem')  { * }
 sub error-function(Real $a) is export { erf(num.new($a.Num)) }
