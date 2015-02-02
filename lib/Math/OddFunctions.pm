16f315c869ee84bb4bf575e93a65eb4b25bc9352
Solomon Foster <colomon@gmail.com>
2015-02-02 07:09:47 -0500

    Add complementary-error-function.

--- lib/Math/OddFunctions.pm
+++ lib/Math/OddFunctions.pm
@@ -5,3 +5,6 @@ use NativeCall;
 
 sub erf(num) returns num is native('libSystem')  { * }
 sub error-function(Real $a) is export { erf(num.new($a.Num)) }
+
+sub erfc(num) returns num is native('libSystem')  { * }
+sub complementary-error-function(Real $a) is export { erfc(num.new($a.Num)) }
